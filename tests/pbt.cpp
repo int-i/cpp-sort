6ef477b1a5e48fad8c64e6cc9103f3660ce6c3b8
Seungjae Park <astro.psj@gmail.com>
2021-07-17 18:59:32 +0900

    Set doctest for unit testing

--- tests/pbt.cpp
+++ tests/pbt.cpp
@@ -1,17 +1,26 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <algorithm>
+#include <vector>
+#include <doctest.h>
 #include <rapidcheck.h>
 
-#include <vector>
-#include <algorithm>
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
 
-int main() {
-  rc::check("double reversal yields the original value",
-            [](const std::vector<int> &l0) {
-              auto l1 = l0;
-              std::reverse(begin(l1), end(l1));
-              std::reverse(begin(l1), end(l1));
-              RC_ASSERT(l0 == l1);
-            });
+TEST_CASE("testing the factorial function") {
+    CHECK(factorial(1) == 1);
+    CHECK(factorial(2) == 2);
+    CHECK(factorial(3) == 6);
+    CHECK(factorial(10) == 3628800);
+}
 
-  return 0;
+TEST_CASE("PBT with RC") {
+    CHECK(rc::check("double reversal yields the original value",
+        [](const std::vector<int> &l0) {
+            auto l1 = l0;
+            std::reverse(begin(l1), end(l1));
+            std::reverse(begin(l1), end(l1));
+            RC_ASSERT(l0 == l1);
+        })
+    );
 }
 
