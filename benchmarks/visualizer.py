8f3fb3340f845f9a61f4fd036c3623e201c4dbb0
Seungjae Park <astro.psj@gmail.com>
2021-10-31 09:10:30 +0900

    Add benchmark visualizer

---
+++ benchmarks/visualizer.py
@@ -0,0 +1,29 @@
+from argparse import ArgumentParser
+import math
+import json
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('path', help='benchmark data json file')
+    parser.add_argument('out', help='benchmark image file')
+    args = parser.parse_args()
+
+    with open(args.path) as file:
+        benchmarks = json.load(file)['benchmarks']
+        data = dict()
+        for benchmark in benchmarks:
+            label, array_size = benchmark['name'].split('/')
+            if label not in data:
+                data[label] = [[], []]
+            data[label][0].append(array_size)
+            data[label][1].append(math.log(benchmark['real_time'], 4))
+        for label, [x, y] in data.items():
+            plt.plot(x, y, label=label)
+        plt.xlabel('array size')
+        plt.ylabel('log(time)')
+        plt.title('Sorting Algorithm Benchmark')
+        plt.legend()
+        plt.savefig(args.out)
+
